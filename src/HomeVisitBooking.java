38a253b382688946eb8e97b58e7f19d616b05fe7
ReemAlmalki-hub <almalkireem@outlook.com>
2025-11-09 21:28:43 +0300

    V4

---
+++ src/HomeVisitBooking.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Reem
+ */
+public class HomeVisitBooking extends Booking {
+    
+    private String address;
+
+    public HomeVisitBooking(String clientName, String date, String time, String address) {
+        super(clientName, date, time);
+        this.address = address;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("\n--- Home Visit Appointment ---");
+        System.out.println("Client: " + clientName);
+        System.out.println("Address: " + address);
+        System.out.println("Date: " + date + " | Time: " + time);
+    }
+} 
